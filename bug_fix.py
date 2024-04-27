87d3f6ad2c16120b62a5d9ed6f3c4b41c21e3637
Vad <trofiki1@gmail.com>
2024-04-27 12:52:51 +0300

    bug fix update 2

--- bug_fix.py
+++ bug_fix.py
@@ -2,8 +2,8 @@
 
 smile_footprint = '\U0001F463'
 
-User_name = 'Max'
-User_age = '5'
-result = f"My name is {User_name}, I am {User_age} years old {smile_footprint}"
+user_name = 'Max'
+user_age = '5'
+result = f"My name is {user_name}, I am {user_age} years old {smile_footprint}"
 
 print(result)
