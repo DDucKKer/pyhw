927b56bc6db67ca5bb32da858799d28244f14ce2
Vad <trofiki1@gmail.com>
2024-04-27 11:19:53 +0300

    bug fix

---
+++ bug fix.py
@@ -0,0 +1,9 @@
+# очікуваний результат у вигляді: My name is David, I am 14 years old👣
+
+smile_footprint = '\U0001F463'
+
+User_name = 'Max'
+User_age = '5'
+result = 'My name is ' + User_name + ", I am " + User_age + ' years old ' + smile_footprint
+
+print(result)
