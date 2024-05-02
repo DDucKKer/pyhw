7f90c27ff820dd7b7257b0064dc78cfe9aebeeb8
Vad <trofiki1@gmail.com>
2024-05-02 14:46:43 +0300

    hw4 gettext/rename update

--- translation.py
+++ translation.py
@@ -1,4 +1,3 @@
-import re
 from gettext import translation
 
 hasht = "#" * 80
@@ -8,18 +7,27 @@ lang = input('Choose the language(en, ru, uk, de, fr): ')
 translator = translation('this_project', 'locale', [lang], fallback=True)
 _ = translator.gettext
 
-name = input(_('Enter your name: ')).capitalize()
-surname = input(_('Enter your surname: ')).capitalize()
-
-name = re.sub("[^A-Za-z]", "", name)
-surname = re.sub("[^A-Za-z]", "", surname)
+name = input(_('Enter your name: '))
+surname = input(_('Enter your surname: '))
 
 
-if (name.find('ван') is True) or (surname.find('вич') is True):
+def strng(word):
+    charlist = " .,!@#$%^&*()_+-=~`\\|?/.,';:[]{}0123456789№\""
+    for char in charlist:
+        if char in word:
+            word = word.replace(char, '')
+    return word
+
+
+name = strng(name)
+surname = strng(surname)
+
+if (name.find('ван') != -1) or (surname.find('вич') != -1):
     print(_("Your name will be changed"))
     name = name.replace('ван', 'ванка')
     surname = surname.replace('вич', '')
 
-print(_("Hello"), name, surname)
+
+print(_("Hello"), name.capitalize(), surname.capitalize())
 
 print(hasht)
