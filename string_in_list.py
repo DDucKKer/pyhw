91c8610f207bd320f7d4f772e7ae7092a3b5fd8b
Vad <trofiki1@gmail.com>
2024-05-07 13:00:17 +0300

    String to list

---
+++ string_in_list.py
@@ -0,0 +1,8 @@
+string = "київ,оДеса     Львів.житоМИР,уЖгОрОд.....ХарКІВ       , слАвУтИч".replace('.', ',').replace(' ', ',')
+
+mass = list(filter(None, string.title().split(',')))
+
+print(mass)
+
+for i in mass:
+    print(f'Я \u2764 {i}!')
