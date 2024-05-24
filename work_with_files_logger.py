d0893275a00efa96f7a63bde15c1ee5e126df8bc
Vad <trofiki1@gmail.com>
2024-05-24 08:45:49 +0300

    work with files

--- work_with_files_logger.py
+++ work_with_files_logger.py
@@ -18,7 +18,7 @@ def logger(file_name='log_data.csv'):
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             with open(file_name, mode='a', encoding='utf-8') as file:
-                file.write(f'{datetime.datetime.now()}; {func.__name__}; {result}\n')
+                file.write(f'{datetime.datetime.now()};{func.__name__};{result}\n')
             return result
         return wrapper
     return wrapper1
